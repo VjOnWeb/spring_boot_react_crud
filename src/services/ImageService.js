3d08f67707e14c638bd9ca953579fcbf378aabec
VjOnWeb <98512909+VjOnWeb@users.noreply.github.com>
2024-03-03 17:28:11 +0530

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch master # Your branch is up to date with 'origin/master'. # # Changes to be committed: #	modified:   src/services/ImageService.js # # Changes not staged for commit: #	modified:   src/services/NoteService.js #

--- src/services/ImageService.js
+++ src/services/ImageService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const REST_API_IMAGE_URL = "http://lccalhost:3838/api/images";
+const REST_API_IMAGE_URL = "http://localhost:9898/api/images";
 
 export const  listAllImages = () => axios.get(REST_API_IMAGE_URL + "/all_images");
 
