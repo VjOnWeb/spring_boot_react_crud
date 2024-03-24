266dfbe42b50e186108016bcb82357a40d59f745
VjOnWeb <98512909+VjOnWeb@users.noreply.github.com>
2024-03-24 22:13:23 +0530

    added icons

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
 
 const HeaderComponent = () => {
   return (
@@ -6,6 +8,7 @@ const HeaderComponent = () => {
       <header>
         <nav className='navbar navbar-dark bg-dark'>
           <a className='navbar-brand' href='/spring_boot_react_crud/'>
+            <FontAwesomeIcon icon={faHome} size="2x" className="me-3 text-primary" />
             User Management System
           </a>
         </nav>
